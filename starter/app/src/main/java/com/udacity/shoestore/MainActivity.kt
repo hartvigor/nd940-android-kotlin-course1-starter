b95ab8c98f564d35b291aac70fca87fc3f24226e
Olav Rui <olav.rui@politiet.no>
2022-08-02 14:50:13 +0200

    Create UIs and fragments. Start Step 7. Login destination

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,6 +2,7 @@ package com.udacity.shoestore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import timber.log.Timber
 
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //val binding = DataBindingUtil.setContentView<Main>()
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
     }
