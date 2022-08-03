2dd13f1d2519eef91d961be96011f212c55286f2
Olav Rui <olav.rui@politiet.no>
2022-08-03 12:01:00 +0200

    Add layouts for all views

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -3,6 +3,7 @@ package com.udacity.shoestore
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 
@@ -10,8 +11,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         //val binding = DataBindingUtil.setContentView<Main>()
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
     }
 }
--- starter/app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment: Fragment() {
     override fun onCreateView(
@@ -12,8 +16,17 @@ class LoginFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding.createLoginButton.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_welcomeFragment)
+        }
+
+        binding.loginButton.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_welcomeFragment)
+        }
+
+        return binding.root
     }
 
 }
